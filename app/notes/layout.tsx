ee1b93a39496edf8a20ba4bbad78defb283ae4f5
Yaroslav13-13 <frilanserwork13@gmail.com>
2025-10-09 14:09:16 +0300

    07-routing-nextjs-fix

--- app/notes/layout.tsx
+++ app/notes/layout.tsx
@@ -1,18 +1,3 @@
-// export default function NotesLayout({
-//   children,
-//   modal,
-// }: {
-//   children: React.ReactNode;
-//   modal: React.ReactNode;
-// }) {
-//   return (
-//     <>
-//       {children}
-//       {modal}
-//     </>
-//   );
-// }
-
 export default function NotesLayout({
   children,
 }: {
