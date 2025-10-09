94265f34f6499347daae870256c151dbf56f0823
Yaroslav13-13 <frilanserwork13@gmail.com>
2025-10-09 13:54:28 +0300

    07-routing-nextjs-fix

--- app/notes/[id]/page.tsx
+++ app/notes/[id]/page.tsx
@@ -1,30 +1,3 @@
-// import {
-//   QueryClient,
-//   dehydrate,
-//   HydrationBoundary,
-// } from "@tanstack/react-query";
-// import { fetchNoteById } from "../../../lib/api";
-// import NoteDetailsClient from "./NoteDetails.client";
-// import TanStackProvider from "../../../components/TanStackProvider/TanStackProvider";
-
-// export default async function NoteDetailsPage({
-//   params,
-// }: {
-//   params: Promise<{ id: string }>;
-// }) {
-//   const { id } = await params;
-//   const queryClient = new QueryClient();
-
-//   await queryClient.prefetchQuery({
-//     queryKey: ["note", id],
-//     queryFn: () => fetchNoteById(id),
-//   });
-
-//   return (
-//     <TanStackProvider>
-//       <HydrationBoundary state={dehydrate(queryClient)}>
-//         <NoteDetailsClient noteId={id} />
-//       </HydrationBoundary>
-//     </TanStackProvider>
-//   );
-// }
+export default function NotePage() {
+  return null;
+}
